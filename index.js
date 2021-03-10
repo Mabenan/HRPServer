6b57f9e68b56a94613f5b8e7c9f75706773e4dd2
Mabenan <doener123@googlemail.com>
2021-03-10 15:41:10 +0100

    changed to use phusion magic

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (typeof PhusionPassenger !== "undefined") {
+  PhusionPassenger.configure({ autoInstall: false });
+}
+
 var express = require("express");
 var cors = require("cors");
 var ParseServer = require("parse-server").ParseServer;
@@ -6,16 +10,22 @@ var app = express();
 function startServer(err) {
   var api = new ParseServer({
     appName: process.env.PARSE_APPNAME || "HRP",
-    databaseURI: process.env.PARSE_DATABASEURI || "mongodb://localhost:27017/hrp",
+    databaseURI:
+      process.env.PARSE_DATABASEURI || "mongodb://localhost:27017/hrp",
     appId: process.env.PARSE_APPID || "ABCDEFG",
     masterKey: process.env.PARSE_MASTERKEY || "ABCDEFG",
     serverURL: process.env.PARSE_SERVERURL || "http://localhost:1337/hrp",
-    publicServerURL: process.env.PARSE_PUBLICSERVERURL || "http://127.0.0.1:1337/hrp",
+    publicServerURL:
+      process.env.PARSE_PUBLICSERVERURL || "http://127.0.0.1:1337/hrp",
     allowHeaders: ["X-Parse-Installation-Id"],
   });
   app.use(cors);
   app.use(process.env.ROUTE || "/hrp", api);
 
-  app.listen(process.env.PORT || "1337");
+  if (typeof PhusionPassenger !== "undefined") {
+    app.listen("passenger");
+  } else {
+    app.listen(process.env.PORT || "1337");
+  }
 }
 startServer();
